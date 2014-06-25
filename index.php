4530fe4f208145fcdd8243a2a84065932f41f499
Facter <franckasaurio@gmail.com>
2014-06-24 21:17:57 -0500

    Solucón del error #303

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 
-<!-- Responsividad segun la resolución -->
+<!-- Responsividad segun la resolución del dispositivo -->
 <META CHARSET="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
